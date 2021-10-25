28f6dbb70279ac6d1c0c6389dc8c2b7e0f0bd749
Rajeev Sharma <talk2rajeev1986@gmail.com>
2021-10-25 22:41:46 +0530

    button component implementation

--- src/Button/index.tsx
+++ src/Button/index.tsx
@@ -8,16 +8,17 @@ interface ButtonInterface {
     buttonType: ButtonType,
     label: string,
     buttonSize: ButtonSize,
-    onclick: (item: React.MouseEvent<HTMLButtonElement, MouseEvent>) => void,
-    disabled?: boolean    
+    onclick: () => void,
+    disabled?: boolean,
+    withBorderRadius?: boolean,    
 }
 
-export const Button = ({ buttonType, buttonSize, label, onclick, disabled }: ButtonInterface) => {
-  
+export const Button = ({ buttonType, buttonSize, label, onclick, disabled, withBorderRadius }: ButtonInterface) => {
+    
     return (
         <button
             type="button"
-            className={`rt-btn rt-btn--${buttonType} rt-btn--${buttonSize}`}
+            className={`rt-btn rt-btn--${buttonType} rt-btn--${buttonSize} ${withBorderRadius ? 'rt-btn--withradius' : ''}`}
             onClick={onclick}
             disabled={disabled}
         >
