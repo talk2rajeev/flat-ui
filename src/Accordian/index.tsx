22ae431a7c8e78812bf2edc8d5cd50db95256c61
Rajeev Sharma <talk2rajeev1986@gmail.com>
2021-11-08 12:27:50 +0530

    Accordian component enhanced

--- src/Accordian/index.tsx
+++ src/Accordian/index.tsx
@@ -1,24 +1,38 @@
-import React, {useState} from 'react';
+import React, {useState, createRef} from 'react';
 import '../assets/css/accordian.min.css';
 
-type ListType<T> = {heading: string, value: string | T};
-
-interface AccordianInterface<T> {
-    list: Array<ListType<T>>,
-    accordianItemComponent?: React.FC<any>,
+interface AccordianInterface {
+    list: Array<string>,
+    onclick: (item: string) => void,
+    children: React.ReactNode,
+    hasArrow: boolean,
+    arrowAlignment: 'left' | 'right',
 }
-
-export const Accordian: React.FC<AccordianInterface<ListType<any>>> = ({ list, accordianItemComponent }: AccordianInterface<ListType<any>>) => {
+export const Accordian: React.FC<AccordianInterface> = ({ list, onclick, children, hasArrow=false, arrowAlignment="left" }: AccordianInterface) => {
     const [state, setstate] = useState<number>(-1)
+
+    const onclickHandler = (index: number) => {
+        setstate(c => {
+            return index;
+        });
+    }
+    
     return (
         <div className="rt-accordian-container">
             {
-                list.map((item: ListType<any>, index: number) => {
+                list.map((item: string, index: number) => {
                     return <div className="rt-accordian-item">
-                            <div className="rt-accordian-item-header" onClick={() => setstate(index)}>{item.heading}</div>
-                            <div className={`rt-accordian-item--${state === index ? 'open' : 'close'}`}>
+                            <div className={`rt-accordian-item-header rt-accordian--${state === index ? 'open' : 'close'} ${hasArrow ? 'rt-accordian-header--arrow rt-accordian-header--arrow-' +arrowAlignment : null}`} onClick={() => onclickHandler(index)}>
+                                {
+                                    hasArrow ? <span className={`rt-accordian-arrow--${arrowAlignment}`}>
+                                        <AccordianArrow />
+                                    </span> : null
+                                }
+                                {item}
+                            </div>
+                            <div className={`rt-accordian-item--${state === index ? 'open' : 'close'}`} >
                             {   
-                                accordianItemComponent ? accordianItemComponent(item.value) :  item.value
+                                children
                             }
                             </div>
                     </div>
@@ -27,3 +41,7 @@ export const Accordian: React.FC<AccordianInterface<ListType<any>>> = ({ list, a
         </div>
     );
 };
+
+const AccordianArrow = () => <svg viewBox="64 64 896 896" focusable="false" data-icon="right" width="1em" height="1em" fill="currentColor" aria-hidden="true" style={{}}>
+    <path d="M765.7 486.8L314.9 134.7A7.97 7.97 0 00302 141v77.3c0 4.9 2.3 9.6 6.1 12.6l360 281.1-360 281.1c-3.9 3-6.1 7.7-6.1 12.6V883c0 6.7 7.7 10.4 12.9 6.3l450.8-352.1a31.96 31.96 0 000-50.4z"></path>
+</svg>
--- src/stories/Accordian/index.stories.tsx
+++ src/stories/Accordian/index.stories.tsx
@@ -6,12 +6,18 @@ function CustomComponent(props: {h: string, v: string}) {
     return <div><h3>{props.h}</h3><p>{props.v}</p></div>
 }
 
-
+const list = ['Panel1', 'Panel2'];
 export default {
     title: 'Accordian',
     component: Accordian,
     argTypes: {
-        list: [{heading: 'panel 1', value: 'values 1' }, {heading: 'panel 2', value: 'values 2' }],
+        list: list,
+        children: <div>this is children</div>,
+        hasArrow: true,
+        arrowAlignment: {
+            options: ['left', 'right'],
+            control: { type: 'radio' }
+        }
     },
 };
 
@@ -20,29 +26,15 @@ const Template = (args) => <Accordian {...args} />;
 
 export const SimpleAccordian = Template.bind({});
 SimpleAccordian.args = {
-    list: [{heading: 'panel 1', value: 'values 1' }, {heading: 'panel 1', value: 'values 1' }]
+    list: ['Panel-1', 'Panel-2'],
+    children: <div>This is SimpleAccordian children</div>,
+    hasArrow: true,
 };
-
 
 export const CustomAccordian = Template.bind({});
 CustomAccordian.args = {
-    list: [
-        {heading: 'panel 1', value: {h: 'content heading1', v: 'content value1'}},
-        {heading: 'panel 2', value: {h: 'content heading2', v: 'content value2'}}
-    ],
-    accordianItemComponent: CustomComponent
-};
-
-
-function CustomComponent1(props: { para: string}) {
-    return <div><p>{props.para}</p></div>
-}
-export const CustomAccordian1 = Template.bind({});
-CustomAccordian1.args = {
-    list: [
-        {heading: 'This is Panel 1', value: {para: 'lorem ipsum is is used as a placeholder for dummy text'}},
-        {heading: 'This is Panel 2', value: {para: 'used as a placeholder lorem ipsum is is used as a placeholder for text'}}
-    ],
-    accordianItemComponent: CustomComponent1
+    list: ['Panel-1.1', 'Panel-2.1'],
+    children: <div>This is CustomAccordian children</div>,
+    hasArrow: true,
 };
 
