d1589cb5fa36beaab792c7e1b04e071cb0d8905f
Rajeev Sharma <talk2rajeev1986@gmail.com>
2021-11-19 16:44:45 +0530

    Toast: vertical aligmnet fix

--- src/Toast/index.tsx
+++ src/Toast/index.tsx
@@ -9,6 +9,9 @@ interface ToasterInterface {
     children?: React.ReactElement,
     toastDuration?: number,
     toastType?: ToastType,
+    closable?: boolean,
+    variation?: 'simple' | 'advance',
+    onclose: (title: string) => void;
 }
 
 interface ToastInterface {
@@ -18,30 +21,34 @@ interface ToastInterface {
 const toastRef = createRef<HTMLDivElement>();
 
 
-
-
-export const Toast = ({ title, description, children, toastDuration, toastType=null }: ToasterInterface) => {
+export const Toast = ({ title, description, children, toastDuration, toastType=null, closable, onclose, variation='simple' }: ToasterInterface) => {
     const closeToaster = () => {
         toastRef.current!.style.display = 'none';
     }
     console.log('Toast called ', title)
 
-    // useEffect(() => {
-    //     let timer: any;
-    //     if (toastDuration) {
-    //         timer = setTimeout(()=>{
-    //             toastRef.current!.style.display = 'none';
-    //         }, toastDuration)
-    //     }
-    //     return () => {
-    //         if (timer) {
-    //             clearTimeout(timer)
-    //         }
-    //     }
-    // }, [])
+    useEffect(() => {
+        let timer: any;
+        if (toastDuration) {
+            timer = setTimeout(()=>{
+                onclose(title)
+            }, toastDuration)
+        } else {
+            if (!closable) {
+                timer = setTimeout(()=>{
+                    onclose(title)
+                }, 4000)
+            }
+        }
+        return () => {
+            if (timer) {
+                clearTimeout(timer)
+            }
+        }
+    }, [])
 
     return (
-        <div className="rt-toast-wrapper" ref={toastRef}>
+        <div className={`rt-toast-wrapper rt-toast-animation-class rt-toast--${variation}`} ref={toastRef}>
             <div>
                 <div className="rt-toast-header">
                     <div className="rt-toast-inner">
@@ -108,31 +115,42 @@ export const toast: ToastInterface = {
 // error: #f5222d
 // warning: #faad14
 
-export const showNotification = ({title, description, toastType=null, duration, closable }: {title: string, description: string, toastType?: ToastType, duration?: number, closable?: boolean}) => {
+interface ShowNotificationInterface {
+    title: string,
+    description: string,
+    toastType?: ToastType,
+    duration?: number,
+    closable?: boolean,
+    variation?: 'simple' | 'advance'
+}
+export const showNotification = ({title, description, toastType=null, duration, closable, variation="simple" }: ShowNotificationInterface) => {
     const el = <div accessKey="sdf">{title}</div>;
     const fragment = document.createDocumentFragment();
     const wrapper = document.createElement('div');
-    wrapper.className="rt-toast-wrapper rt-toast-animation-class";
+    wrapper.className=`rt-toast-wrapper rt-toast-animation-class rt-toast--${variation}`;
 
     const headerEl = header(title, close, toastType);
     const bodyEl = body(description);
 
     wrapper.appendChild(headerEl);
     wrapper.appendChild(bodyEl);
-    
     fragment.appendChild(wrapper);
+    
+    let fromTop = getToastPositionFromTop();
+    wrapper.style.top = `${fromTop}px`;
+
     document.body.appendChild(fragment);
 
     let timer: any;
 
     if (duration) {
         timer = setTimeout(()=>{
-            document.body.removeChild(wrapper);
+            close();
         }, duration)
     } else {
         if (!closable) {
             timer = setTimeout(()=>{
-                document.body.removeChild(wrapper);
+                close();
             }, 4000)
         }
     }
@@ -140,6 +158,13 @@ export const showNotification = ({title, description, toastType=null, duration, 
     function close() {
         document.body.removeChild(wrapper);
         clearTimeout(timer);
+    }
+
+    function getToastPositionFromTop(): number {
+        let fromTop = 0;
+        const toastElArr = document.body.querySelectorAll('.rt-toast-wrapper');
+        toastElArr.forEach(toastEl => fromTop+=toastEl.clientHeight +20)
+        return fromTop;
     }
 }
 
@@ -153,8 +178,22 @@ const header = (title: string, close: ()=>void, toastType: ToastType) => {
     headerTitleEl.className= "rt-toast-title";
     headerTitleEl.textContent = title;
     if (toastType) {
-        const toastIconEl = errorIcon();
-        headerEl.appendChild(toastIconEl);
+        if (toastType === 'error') {
+            const toastIconEl = errorIcon();
+            headerEl.appendChild(toastIconEl);
+        }
+        if (toastType === 'info') {
+            const toastIconEl = warningInfoIcon('info');
+            headerEl.appendChild(toastIconEl);
+        }
+        if (toastType === 'warning') {
+            const toastIconEl = warningInfoIcon('warning');
+            headerEl.appendChild(toastIconEl);
+        }
+        if (toastType === 'success') {
+            const toastIconEl = successIcon();
+            headerEl.appendChild(toastIconEl);
+        }
     }
     headerEl.appendChild(headerTitleEl);
     headerEl.appendChild(closeElWrapper);
@@ -184,6 +223,15 @@ const closeIcon = (close: ()=>void) => {
     return closeElWrapper;
 }
 
+const successIcon = () => {
+    const successElWrapper = document.createElement('span');
+    successElWrapper.className="rt-icon rt-icon--error";
+    successElWrapper.innerHTML = `<svg width="24" height="24" className="mr-2" viewBox="0 0 24 24">
+        <path d="M12,2A10,10 0 0,1 22,12A10,10 0 0,1 12,22A10,10 0 0,1 2,12A10,10 0 0,1 12,2M11,16.5L18,9.5L16.59,8.09L11,13.67L7.91,10.59L6.5,12L11,16.5Z" fill="#52c41a"></path>
+    </svg>`;
+    return successElWrapper;
+}
+
 const errorIcon = () => {
     const errorElWrapper = document.createElement('span');
     errorElWrapper.className="rt-icon rt-icon--error";
@@ -192,6 +240,17 @@ const errorIcon = () => {
         <path d="M512 65C264.6 65 64 265.6 64 513s200.6 448 448 448 448-200.6 448-448S759.4 65 512 65zm0 820c-205.4 0-372-166.6-372-372s166.6-372 372-372 372 166.6 372 372-166.6 372-372 372z"></path>
     </svg>`;
     return errorElWrapper;
+}
+
+const warningInfoIcon = (iconType: 'warning' | 'info') => {
+    const fillColor = iconType === 'warning' ? '#faad14' : '#1890ff';
+    const warningInfoElWrapper = document.createElement('span');
+    warningInfoElWrapper.className="rt-icon rt-icon--error";
+    warningInfoElWrapper.innerHTML = `<svg viewBox="64 64 896 896" focusable="false" data-icon="exclamation-circle" width="1em" height="1em" fill=${fillColor} aria-hidden="true">
+        <path d="M512 64C264.6 64 64 264.6 64 512s200.6 448 448 448 448-200.6 448-448S759.4 64 512 64zm0 820c-205.4 0-372-166.6-372-372s166.6-372 372-372 372 166.6 372 372-166.6 372-372 372z"></path>
+        <path d="M464 688a48 48 0 1096 0 48 48 0 10-96 0zm24-112h48c4.4 0 8-3.6 8-8V296c0-4.4-3.6-8-8-8h-48c-4.4 0-8 3.6-8 8v272c0 4.4 3.6 8 8 8z"></path>
+    </svg>`;
+    return warningInfoElWrapper;
 }
 
 
--- src/stories/Toast/index.stories.tsx
+++ src/stories/Toast/index.stories.tsx
@@ -13,14 +13,22 @@ storiesOf("Toast", module)
             }
             
             return <div>
-                { showtoast && <Toast title="this is title" toastDuration={5000}>
+                { showtoast && <Toast title="this is title" closable onclose={()=> setShowtoast(false)}>
                     <div>FASDFASDF</div>
                 </Toast> }
-                <Button withBorderRadius label="showToast" buttonType="secondary" buttonSize="medium"  onclick={() => toast.show({title:"this is title"})} /> &nbsp;
+                <Button withBorderRadius label="showToast" buttonType="secondary" buttonSize="medium"  onclick={() => setShowtoast(true)} /> &nbsp;
                 <Button withBorderRadius label="Fail" buttonType="primary" buttonSize="medium"  onclick={() => showNotification({title: "Fail", description: "Sorry, the process failed"})} /> &nbsp;
-                <Button withBorderRadius label="Success" buttonType="primary" buttonSize="medium"   onclick={() => showNotification({title: "success", description: "success, the process succed"})} /> &nbsp;
-                <Button withBorderRadius label="Info" buttonType="primary" buttonSize="medium"   onclick={() => showNotification( {title: "info", description: "this info is regarding process", closable:true} ) } /> &nbsp;
-                <Button withBorderRadius label="Error" buttonType="primary" buttonSize="medium"   onclick={() => showNotification({title: "Error", description: "Sorry Error occured, the process failed. Pleae check with server administrator.", toastType:"error", closable:true})} /> 
+                <Button withBorderRadius label="Success" buttonType="primary" buttonSize="medium"   onclick={() => showNotification({title: "success", description: "success, the process succed", toastType:"success", variation: "advance"})} /> &nbsp;
+                <Button withBorderRadius label="Info" buttonType="primary" buttonSize="medium"   onclick={() => showNotification( {title: "info", description: "this info is regarding process", closable:true, toastType:"info", variation: "advance"} ) } /> &nbsp;
+                <Button withBorderRadius label="Warning" buttonType="primary" buttonSize="medium"   onclick={() => showNotification( {title: "info", description: "this info is regarding process", closable:true, toastType:"warning"} ) } /> &nbsp;
+                <Button withBorderRadius label="Error" buttonType="primary" buttonSize="medium"   onclick={() => showNotification({title: "Error", description: "Sorry Error occured, the process failed. Pleae check with server administrator.", toastType:"error", closable:true})} />
+                <span>this is the main reson why i use react js over javascript</span>
+                <br /> 
+                <br />
+                <br />
+                <br />
+                <br />
+                <Button withBorderRadius label="Error" buttonType="primary" buttonSize="medium"   onclick={() => showNotification({title: "Error", description: "Sorry Error occured, the process failed. Pleae check with server administrator.", toastType:"error", closable:true})} />
             </div>
         }
     );
