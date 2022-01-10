210fbe2b0f1321daddd7b1a3304ef9714c3d9203
Rajeev Sharma <talk2rajeev1986@gmail.com>
2022-01-10 16:02:08 +0530

    Tab storybook modified

--- src/stories/Tab/index.stories.tsx
+++ src/stories/Tab/index.stories.tsx
@@ -6,13 +6,35 @@ const RenderChild = ({activeTab}) => {
     return <div>{activeTab}</div>
 }
 
+const tabList = ['One', 'Two', 'Three'];
+const RenderChild1 = ({activeTab}) => { 
+    switch(activeTab) {
+        case 'One':
+            return <h1>One</h1>
+        case 'Two':
+            return <p>One</p>    
+        case 'Three':
+            return <h4>Three</h4>    
+        default:
+            return <p>{activeTab}</p>;    
+    }
+}
+
 storiesOf("Tab", module)
 .add("Simple Tab",() => {
-    const Item = (item: any) => <div>{item}</div>
     return <div>
         <Tab
             tabList={ ['One', 'Two', 'Three'] }
             renderChild={RenderChild}
+            defaultTab="One"
+        />
+    </div>
+})
+.add("Custom Tab child",() => {
+    return <div>
+        <Tab
+            tabList={ tabList }
+            renderChild={RenderChild1}
             defaultTab="One"
         />
     </div>
