9a42b667fde1f4ed55d0f2493c6b0d7ddfb246f9
Rajeev Sharma <talk2rajeev1986@gmail.com>
2022-01-12 00:04:36 +0530

    dateSelect event added

--- src/DatePicker/index.tsx
+++ src/DatePicker/index.tsx
@@ -1,18 +1,23 @@
-import React, { useState, useRef, useEffect } from 'react';
+import React, { useState } from 'react';
 import '../assets/css/datepicker.min.css';
 
 interface DatePickerInterface {
     defaultDate: string,
+    onDateSelect: (date: string) => void
 }
 
 
-export const DatePicker = ({ defaultDate }: DatePickerInterface) => {
+export const DatePicker = ({ defaultDate, onDateSelect }: DatePickerInterface) => {
     const [selectedDate, setSelectedDate] = useState(defaultDate)
     
+    const onselect = (e: React.ChangeEvent<HTMLInputElement>) => {
+        setSelectedDate(e.target.value);
+        onDateSelect(e.target.value)
+    }
 
     return (
         <div className="rt-datepicker-wrapper">
-            <input type="date" value={selectedDate} onChange={e=>setSelectedDate(e.target.value)}/>
+            <input className="rt-datepicker" type="date" value={selectedDate} onChange={onselect}/>
         </div>
     );
 };
