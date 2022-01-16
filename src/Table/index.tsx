b5828cb1d5c7a461e8b6456fc72b97500cb674be
Rajeev Sharma <talk2rajeev1986@gmail.com>
2022-01-16 18:06:38 +0530

    Table component created

---
+++ src/Table/index.tsx
@@ -0,0 +1,17 @@
+import React, { useState } from 'react';
+import '../assets/css/table.min.css';
+
+export interface TableInterface {
+    data: Array<any>,
+    bordered?: boolean,
+}
+
+
+export const DatePicker = ({ data, bordered }: TableInterface) => {
+    
+    return (
+        <div className="rt-table-wrapper">
+            
+        </div>
+    );
+};
