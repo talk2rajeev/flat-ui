768d8fd7444f368490a1634d7730f625530352ff
Rajeev Sharma <talk2rajeev1986@gmail.com>
2022-01-19 13:29:02 +0530

    component list commented

--- src/index.tsx
+++ src/index.tsx
@@ -15,3 +15,49 @@ export {
     Card,
     BreadCrumb,
 }
+
+/*
+sleek-ui components
+
+react-slick-ui
+
+InputText (with icon & it’s alignment, border=yes/no)
+InputNumber
+Button
+BreadCrumb
+Card
+Checkbox
+Radio
+Menu (inline, stacked)
+Accordian
+Step
+List, ListItem
+Tab
+
+
+Calendar
+Carousel
+DialogBox
+ConfirmbBox
+DatePicker
+TimePicker
+Dropdown
+Autocomplete
+Rating
+
+SideMenu (with child)
+MegaMenu
+
+
+Loader
+Toast
+Tooltip
+
+Grid
+
+Panel
+
+Table
+Alert
+ProgressBar
+*/
